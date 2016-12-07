0251f78aac89f93637f7e89002844743ccde12a7
nikita.wankhade <nikita.wankhade@abc.com>
2016-12-07 15:26:28 +0530

    test

--- CounterWebApp/src/test/java/com/sample/controller/BaseControllerTest.java
+++ CounterWebApp/src/test/java/com/sample/controller/BaseControllerTest.java
@@ -1,6 +1,7 @@
 package com.sample.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -29,7 +30,20 @@ public class BaseControllerTest extends TestCase {
 	 * Rigourous Test :-)
 	 */
 	public void testApp() {
+		System.out.println();
 		assertTrue(true);
+	}
+	
+	public void testWelcome(){
+		BaseController bc = new BaseController();
+		bc.welcome(new ModelMap());
+		assertEquals("index","index");
+	}
+	
+	public void testWelcomeName(){
+		BaseController bc = new BaseController();
+		bc.welcomeName("index",new ModelMap());
+		assertEquals("index","index");
 	}
 
 }
